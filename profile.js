4282c19773ff96f1c336144d5c52d56a85c524fb
Morten E. <0x1F0FFF@users.noreply.github.com>
2025-05-15 19:27:01 +0300

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,7 +1,6 @@
 const fNameSpan = document.querySelector('#fName');
 const lNameSpan = document.querySelector('#lName');
 const ageSpan = document.querySelector('#age');
-const skillsList = document.querySelector('#skills');
 const phoneSpan = document.querySelector('#phone');
 const emailSpan = document.querySelector('#email');
 const addressSpan = document.querySelector('#address');
@@ -15,13 +14,6 @@ window.onload = () => {
         fNameSpan.textContent = data.fName;
         lNameSpan.textContent = data.lName;
         ageSpan.textContent = data.age;
-    
-        for (let i = 0; i < data.skills.length; i++){
-           let li = document.createElement('li');
-           li.innerHTML = data.skills[i];
-
-           skillsList.appendChild(li);
-        }
 
         phoneSpan.textContent = data.phone;
         emailSpan.textContent = data.email;
